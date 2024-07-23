0a19b8572b5dcff2cce82eea6e2e9d161d9ee82b
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-07-22 20:40:58 -0400

    Updating mock

--- app/src/main/java/com/example/tasks_jonathan/Mock.kt
+++ app/src/main/java/com/example/tasks_jonathan/Mock.kt
@@ -4,13 +4,6 @@ object Mock {
     var TODO_LIST: List<Task> = listOf(
         Task("Do the laundry", false),
         Task("Clean the kitchen", true),
-        Task("Vacuum the living room", false),
-        Task("Wash the dishes", true),
-        Task("Take out the trash", false),
-        Task("Water the plants", true),
-        Task("Mow the lawn", false),
-        Task("Organize the garage", true),
-        Task("Dust the shelves", false),
-        Task("Clean the windows", true)
+        Task("Vacuum the living room", false)
     )
 }
